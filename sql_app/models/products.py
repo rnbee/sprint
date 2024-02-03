4aac3a26b57713dd69a83cdcef561416376af255
rnbee <sonakarpovic873@gmail.com>
2024-02-04 11:29:49 +1200

    chge models

--- sql_app/models/products.py
+++ sql_app/models/products.py
@@ -5,12 +5,13 @@ class Product(Base):
     __tablename__ = 'product'
 
     id_product = Column(Integer, primary_key=True, autoincrement=True)
-    amount = Column(Integer, default=0)
-    title = Column(String(length=50), nullable=False)
-    id_distributor = Column(Integer, ForeignKey('distributor.id_distributor'))
-    id_category = Column(Integer, ForeignKey('category.id_category'))
+    product_name = Column(String(length=50), nullable=False)
     price = Column(Numeric(precision=10, scale=2), index=True)
     img = Column(LargeBinary, nullable=True)
+    description = Column(String(length=50), nullable=False)
+    id_distributor = Column(Integer, ForeignKey('distributor.id_distributor'))
+    id_category = Column(Integer, ForeignKey('category.id_category'))
 
     category = relationship('Category', back_populates='products')
     distributors = relationship('Distributor', back_populates='product')
+    warehouses = relationship('Warehouse', back_populates='product')
--- sql_app/models/warehous.py
+++ sql_app/models/warehous.py
@@ -5,8 +5,9 @@ class Warehouse(Base):
     __tablename__ = 'warehouse'
 
     id_warehouse = Column(Integer, primary_key=True, autoincrement=True)
-    location = Column(String(length=50), nullable=False)
     id_distributor = Column(Integer, ForeignKey('distributor.id_distributor'))
+    location = Column(String(length=50), nullable=False)
+    amount = Column(Integer, nullable=False)
 
     distributor = relationship('Distributor', back_populates='warehouses')
     product = relationship('Product', back_populates='warehouses')
